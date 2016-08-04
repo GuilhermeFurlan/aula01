7048d7ecfa274f5d4099dc29c8a26ac591b10260
Guilherme Furlan <guilherme.furlan@b2wdigital.com>
2016-08-04 09:07:17 -0300

    Adicionando caixa de dialogo para passagem de parametro

--- Saudacao.java
+++ Saudacao.java
@@ -1,7 +1,16 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 class Saudacao {
 
   public static void main(String args[]) {
-    System.out.println(args[0] 
+    //System.out.println("Digite o nome do aluno : ");
+  
+    Scanner entrada = new Scanner(System.in);
+    // Aqui criamos o objeto que recebera o nome do aluno
+    //String nome = entrada.nextLine();
+    String nome = JOptionPane.showInputDialog(" Digite o nome do alunooo : ");
+  
+    System.out.println(nome 
                        + " Bem vindo ao curso de java");
   }
 }
